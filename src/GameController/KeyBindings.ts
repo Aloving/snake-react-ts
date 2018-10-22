3a5462a4bc3911c1dac39971839318c76be4fccd
Kartushin Aleksei <akartushin@yamoney.ru>
2018-10-23 00:32:21 +0300

    Добавляет GameController

---
+++ src/GameController/KeyBindings.ts
@@ -0,0 +1,19 @@
+import {Direction} from '../Snake/direction.enum';
+
+export const browserBinds = {
+    /**
+     * arrows
+     */
+    38: Direction.UP,
+    39: Direction.RIGHT,
+    40: Direction.DOWN,
+    37: Direction.LEFT,
+
+    /**
+     * WASD
+     */
+    87: Direction.UP,
+    65: Direction.LEFT,
+    83: Direction.DOWN,
+    68: Direction.RIGHT,
+};
---
+++ src/GameController/index.ts
@@ -0,0 +1,44 @@
+import {browserBinds} from './KeyBindings';
+
+import {Direction} from '../Snake/direction.enum';
+import {GameControllerType} from './GameControllerType.enum';
+
+export class GameController {
+    constructor(private type: GameControllerType) {}
+
+    /**
+     * Получение обработчика
+     * @returns {(e: KeyboardEvent) => Direction}
+     */
+    public handler() {
+        return this.handlersFactory();
+    }
+
+    /**
+     * Получение нужного обработчика по типу
+     */
+    private handlersFactory() {
+        switch (this.type) {
+            case GameControllerType.BROWSER: {
+                return this.browserHandler
+            }
+
+            default: {
+                return this.browserHandler
+            }
+        }
+    }
+
+    /**
+     * Простой браузерный обработчик, получение направление в зависимости от нажатой клавиши
+     * @param {KeyboardEvent} e Нажатая клавиша
+     * @returns {Direction}
+     */
+    private browserHandler = (e: KeyboardEvent): Direction => {
+        const key = e.keyCode;
+
+        console.log(key);
+
+        return browserBinds[key] || '';
+    }
+}
