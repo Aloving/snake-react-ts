9b1b4a068d78591306491d6d5f02b02243a56250
Kartushin Aleksei <akartushin@yamoney.ru>
2018-10-22 17:01:13 +0300

    Добавляет класс Board

---
+++ src/Board/index.ts
@@ -0,0 +1,49 @@
+import Coordinates from '../interfaces/Coordinates';
+import SizeProperties from '../interfaces/SizeProperties';
+
+/**
+ * Класс игрового поля
+ */
+export class Board {
+    /**
+     * Сладость, может быть яблоко, груша или что угодно
+     */
+    private treat: Coordinates;
+
+    constructor(private sizeProperties: SizeProperties) {
+        this.treat = {x: 13, y: 14};
+    }
+
+    /**
+     * Сгенерировать новую позицию для сладости
+     */
+    public updateTreat() {
+        this.treat = this.calcRandomPosition()
+    }
+
+    /**
+     * Вернуть размеры поля
+     */
+    public getSizeProperties() : SizeProperties {
+        return this.sizeProperties;
+    }
+
+    /**
+     * Получить координаты сладости
+     */
+    public getTreat(): Coordinates {
+        return this.treat;
+    }
+
+    /**
+     * Сгененировать новые координаты
+     */
+    private calcRandomPosition(): Coordinates {
+        const {width, height} = this.sizeProperties;
+
+        return {
+            x: Math.floor(Math.random() * height),
+            y: Math.floor(Math.random() * width),
+        }
+    }
+}
